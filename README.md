3a2ae72b3efc41e95cf58007e3e4852c6c335df4
Joppe Blondel <joppe@blondel.nl>
2017-03-25 19:45:21 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+## idea's
+
+statement to check if 'this' collides with 'A'
+x<A.x+12 && x+12>A.x && y<A.y+12 && y+12>A.y
+
+## issue's
+
+Override of draw() does not work
--- src/PowerPellet.cpp
+++ src/PowerPellet.cpp
@@ -1,6 +1,9 @@
 #include "PowerPellet.h"
 
+#include <stdio.h>
+
 void PowerPellet::draw(){
 	if(sprite.sprite == pellet) sprite.sprite = pelletbig;
 	else sprite.sprite = pellet;
+	printf("DRAW\r\n");
 }
